6b972a3c9d50c09559ac59fc1d4ee6e3fadf1fad
Sworup Jangam <sworupjangamdh@gmail.com>
2026-01-23 14:58:54 +0545

    Cloudflared added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const BACKEND_URL = "https://reed-employment-match-warner.trycloudflare.com";
+
 console.log("Loading MiniLM...");
 const extractor = await pipeline(
   "feature-extraction",
